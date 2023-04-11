888bc7d1eb2fd607b7dbcb6817cb8ad64084c49d
William Matlock <55752443+wtmatlock@users.noreply.github.com>
2023-04-11 14:31:19 +0100

    Add files via upload
    now tsv instead of csv

--- fastaLengths.py
+++ fastaLengths.py
@@ -2,11 +2,11 @@ import sys
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 fastaFile = open(sys.argv[1], 'r')
-lengthsFile= open('./lengths.csv', 'w')
+lengthsFile= open('./lengths.tsv', 'w')
 
 for name, seq in SimpleFastaParser(fastaFile):
     seqLength = len(seq)
-    lengthsFile.write(name + ',' + str(seqLength) + '\n')
+    lengthsFile.write(name + '\t' + str(seqLength) + '\n')
 
 fastaFile.close()
 lengthsFile.close()
