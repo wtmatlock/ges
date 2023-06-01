2090c4463ce89a8cd93854dc9e614c04e43ca78b
William Matlock <55752443+wtmatlock@users.noreply.github.com>
2023-06-01 12:43:18 +0100

    Update extractIntegrase.py
    Handles integrase strand; also writes a translated protein FASTA

--- extractIntegrase.py
+++ extractIntegrase.py
@@ -1,24 +1,36 @@
 import os
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna, generic_protein
 
-# open the .tsv file and read in the data
-with open('sys.argv[1], 'r') as f:
+# open the TSV file and read in the data
+with open('/Users/willmatlock/Desktop/integrase-positions.tsv', 'r') as f:
     data = [line.strip().split('\t') for line in f.readlines()]
 
-# loop over each row in the .tsv file
+# loop over each row in the TSV file
 for row in data:
-    # extract the data
+    # extract the relevant data
     name = row[0]
     start = int(row[1])
     end = int(row[2])
+    strand = int(row[3])
 
-    # open the corresponding .fasta file and read in the sequence
-    with open(f'./{name}.fasta', 'r') as f:
+    # open the corresponding FASTA file and read in the sequence
+    with open(f'/Users/willmatlock/Desktop/ges/rescomp-backup/contigs/{name}.fasta', 'r') as f:
         fasta_data = f.readlines()
     seq = ''.join(fasta_data[1:]).replace('\n', '')
 
     # extract the desired subsequence
     subseq = seq[start-1:end]
 
-    # Write the subsequence to a new .fasta file
-    with open(f'./contigs/{name}-integrase.fasta', 'w') as f:
-        f.write(f'>{name}_integrase\n{subseq}\n')
+    # reverse complement the subsequence if the strand is -1
+    if strand == -1:
+        subseq = str(Seq(subseq).reverse_complement())
+
+    # write the subsequence to a new FASTA file
+    with open(f'/Users/willmatlock/Desktop/integrase-sequences/{name}-integrase.fasta', 'w') as f:
+        f.write(f'>{name}-integrase\n{subseq}\n')
+
+    # translate the subsequence to protein and write it to a new FASTA file
+    prot_seq = Seq(subseq, generic_dna).translate()
+    with open(f'/Users/willmatlock/Desktop/integrase-sequences/{name}-integrase-prot.fasta', 'w') as f:
+        f.write(f'>{name}-integrase-prot\n{prot_seq}\n')
